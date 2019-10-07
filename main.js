19eb7c63ceb1f689c0459eb049e1d4a1aa07280b
Anja Weigelt <aweigelt@ufl.edu>
2019-10-07 19:36:45 -0400

    Add dynamic location and weather data to JS

--- main.js
+++ main.js
@@ -5,6 +5,19 @@ xmlhttp.onreadystatechange = function() {
 
         //Code Dealing With The API Data Goes Here
 
+        //Create Variable for Location and Weather Information
+
+        var cityName = document.createTextNode(apiResult.name);
+        var weatherDescription = document.createTextNode(apiResult.weather[0].description);
+
+        //Add location and weather information to screen
+
+        var cityLocation = document.querySelector('h2');
+        var weatherLocation = document.querySelector('h3');
+
+        cityLocation.appendChild(cityName);
+        weatherLocation.appendChild(weatherDescription);
+
     }
 };
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=33897,us&appid=6efff70fe1477748e31c17d1c504635f', true);
